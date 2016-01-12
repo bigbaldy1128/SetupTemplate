4366396978724fce79ba35f93be6f960a82b7304
bigbaldy <bigbaldy@163.com>
2016-01-12 10:26:45 +0800

    fixed

---
+++ SetupTemplate/InstallInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetupTemplate
+{
+    [Serializable]
+    public class InstallInfo
+    {
+        /// <summary>
+        /// 版本号
+        /// </summary>
+        /// <returns></returns>
+        public string Version { get;set; }
+        /// <summary>
+        /// 安装路径
+        /// </summary>
+        /// <returns></returns>
+        public string InstallationPath { get;set; }
+    }
+}
---
+++ SetupTemplate/Log.cs
@@ -0,0 +1,29 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetupTemplate
+{
+    public class Log
+    {
+        public static ILogger Instance = LogManager.GetCurrentClassLogger();
+
+        static Log()
+        {
+            var config = new LoggingConfiguration();
+            var fileTarget = new FileTarget()
+            {
+                Layout = "${longdate} ${uppercase:[${level}]} - ${message} ${exception:format=tostring}",
+                FileName = "${basedir}/logs/${shortdate}.log",
+                Encoding = Encoding.UTF8
+            };
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, fileTarget));
+            LogManager.Configuration = config;
+        }
+    }
+}
